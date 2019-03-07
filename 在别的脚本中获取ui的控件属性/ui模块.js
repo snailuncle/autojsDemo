70876db71a5e91b7638b0a2e59cc4988999d08be
snailuncle <1789500304@qq.com>
2019-03-07 14:17:49 +0800

    first commit

---
+++ 在别的脚本中获取ui的控件属性/ui模块.js
@@ -0,0 +1,7 @@
+"ui";
+ui.layout(
+  <vertical>
+    <button id="myButton" text="this is a button"></button>
+  </vertical>
+)
+module.exports=ui.myButton
---
+++ 在别的脚本中获取ui的控件属性/获取ui模块中按钮的文本.js
@@ -0,0 +1,3 @@
+"ui";
+var ui的引用=require('./ui模块.js')
+log(ui的引用.text())
