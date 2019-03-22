71fc72c4f493e46f98b9d9496a4c043b907f35b6
snailuncle <1789500304@qq.com>
2019-03-22 12:17:06 +0800

    daily-3-20-1

--- 工具箱/脚本排队/排队运行脚本.js
+++ 工具箱/脚本排队/排队运行脚本.js
@@ -1,3 +1,9 @@
+/**
+ * 作者:  家
+ * QQ:    203118908
+ * 功能:   多个脚本排队运行
+ */
+// 当脚本退出时,添加一个parent.go(),parent是外部传给脚本的参数
 function codeWhenExitFunction() {
   var args = engines.myEngine().execArgv;
   // log(args);
@@ -7,9 +13,11 @@ function codeWhenExitFunction() {
     parent.go()
   });
 }
+// 把codeWhenExitFunction代码的函数体提取出来,去掉花括号
 var codeWhenExit = codeWhenExitFunction.toString().replace(/function *?codeWhenExitFunction(.*?){/, '').replace(/}[\s\S]?$/, '')
 
 function Task(name, scriptPaths) {
+  // num是一个计数器
   var num = function () {
     var count = 0
     return function () {
@@ -18,14 +26,16 @@ function Task(name, scriptPaths) {
   }()
   this.num = num
   this.name = name
-  this.scriptPaths = scriptPaths // scripts是数组,存放脚本路径
+  // scripts是数组,存放脚本路径
+  this.scriptPaths = scriptPaths
+  // 先读取指定路径的脚本,添加退出事件的函数,就可以按照顺序调用下一个脚本了
   this.go = function () {
     var serialNumber = this.num()
     // log('serialNumber')
     // log(serialNumber)
-    if(serialNumber>=this.scriptPaths.length){
+    if (serialNumber >= this.scriptPaths.length) {
       log('脚本全部运行完毕')
-      return ;
+      return;
     }
     var path = this.scriptPaths[serialNumber]
     // log('path')
