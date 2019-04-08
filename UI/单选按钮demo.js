b6f4057a121fe4b94064d0baad1435bab498b803
snailuncle <1789500304@qq.com>
2019-04-08 18:03:05 +0800

    daily-4-8-1

--- UI/单选按钮demo.js
+++ UI/单选按钮demo.js
@@ -16,26 +16,39 @@ ui.layout(
 </horizontal>
 )
 
+单选按钮集合=[]
 ui.post(
   function(){
     var count=ui.fbName.getChildCount()
     for(var i=0;i<count;i++){
       var view=ui.fbName.getChildAt(i)
-      view.setId(i)
+      var id=view.id
+      var content=view.getText().toString()
+      单选按钮集合.push({
+        num:i,
+        id:id,
+        content:content,
+      })
     }
+    log(单选按钮集合)
   }
 )
 
+
+function getAttr(obj) {
+  var attrs = []
+  for (var k in obj) {
+    attrs.push(k)
+  }
+  attrs.sort()
+  log( attrs)
+}
+
 ui.fbName.setOnCheckedChangeListener(
   function (radioGroup,id){
-    var count=radioGroup.getChildCount()
-    var id=id % count -1
-    if(id==-1){
-      id=count-1
-    }
-    id=radioGroup.getCheckedRadioButtonId()
-    content=radioGroup.getChildAt(id).getText()
-    var msg=util.format('id=%s, content=%s', id, content)
+    var myIdStartFrom0=id-单选按钮集合[0].id
+    var content=radioGroup.getChildAt(myIdStartFrom0).getText().toString()
+    var msg=util.format('序号=%s, 内容=%s', myIdStartFrom0, content)
     toastLog(msg)
   }
 )
