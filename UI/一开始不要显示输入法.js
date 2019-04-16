8dfb4025a190879224b4153dd8802ba7565c5572
snailuncle <1789500304@qq.com>
2019-04-16 12:51:57 +0800

    daily-4-16-1

--- UI/一开始不要显示输入法.js
+++ UI/一开始不要显示输入法.js
@@ -1,1 +1,1 @@
-activity.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+activity.getWindow().setSoftInputMode(android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
