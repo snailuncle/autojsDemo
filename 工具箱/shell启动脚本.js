2fb9377166367b3617962668291e255b7185bd86
snailuncle <1789500304@qq.com>
2019-04-28 16:31:57 +0800

    daily-4-28

--- 工具箱/shell启动脚本.js
+++ 工具箱/shell启动脚本.js
@@ -4,3 +4,11 @@ function runScript(path){
 }
 path='/sdcard/脚本/2.js'
 runScript(path)
+
+function runScriptPro1(path) {
+  shell("am start -n org.autojs.autojspro/org.autojs.autojs.external.open.RunIntentActivity -d file://" + path + " -t application/x-javascript", true)
+}
+
+function runScriptPro2(path) {
+  shell("am start -n org.autojs.autojspro/org.autojs.autojs.external.open.RunIntentActivity -d file://" + path + " -t text/javascript", true)
+}
