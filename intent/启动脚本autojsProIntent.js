79569f9e06c32e0ef0862dbebca951cf3b167b82
snailuncle <1789500304@qq.com>
2019-05-12 17:11:08 +0800

    启动脚本proIntent

---
+++ intent/启动脚本autojsProIntent.js
@@ -0,0 +1,6 @@
+app.startActivity({
+  packageName : "org.autojs.autojspro",
+  className : "org.autojs.autojs.external.open.RunIntentActivity",
+  data : "/sdcard/脚本/2.js",
+  type : "application/x-javascript"
+});
