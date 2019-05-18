df6b25fdcee63aadff2c1452d0e2978b30509575
snailuncle <1789500304@qq.com>
2019-05-18 13:26:17 +0800

    daily-5-18-气泡监听和移除

---
+++ events/气泡监听和移除.js
@@ -0,0 +1,13 @@
+events.observeToast();
+events.onToast(function(toast){
+    log("Toast内容: " + toast.getText() + " 包名: " + toast.getPackageName());
+});
+
+setTimeout(
+  function(){
+    log(events.eventNames())
+    events.removeAllListeners()
+    log('所有监听移除')
+    log(events.eventNames())
+  },3000
+)
--- 工具箱/监听一次toast.js
+++ 工具箱/监听一次toast.js
@@ -2,3 +2,13 @@ events.observeToast();
 events.once('toast', function () {
   log('bbbbbbbbbbbbbbbb')
 })
+printObj(events)
+function printObj(obj){
+  var arr=[]
+  for(var k in obj){
+    arr.push(k)
+  }
+  arr.sort()
+  log(arr)
+  return arr
+}
